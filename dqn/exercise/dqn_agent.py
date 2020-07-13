7f99f223a2da7e2d50416d4dd4148add309571f3
Arthur Faisman <>
2020-07-13 10:46:50 -0400

    simplify TF model definitions

--- dqn/exercise/dqn_agent.py
+++ dqn/exercise/dqn_agent.py
@@ -5,7 +5,7 @@ from collections import namedtuple, deque
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
-
+from tensorflow.python.framework.ops import disable_eager_execution
 
 
 BUFFER_SIZE = int(1e5)  # replay buffer size
@@ -27,13 +27,14 @@ class Agent():
             action_size (int): dimension of each action
             seed (int): random seed
         """
+        #disable_eager_execution()
         self.state_size = state_size
         self.action_size = action_size
         tf.random.set_seed(seed)
 
         # Q-Network
-        self.localActionModels, self.localFullModel = self.create_network(state_size, action_size)
-        self.targetActionModels, self.targetFullModel = self.create_network(state_size, action_size)
+        self.localModel = self.create_network(state_size, action_size)
+        self.targetModel = self.create_network(state_size, action_size)
 
         self.optimizer = keras.optimizers.Adam(learning_rate=LR)
         self.loss_fn = keras.losses.MeanSquaredError()
@@ -48,16 +49,9 @@ class Agent():
 
         hiddenLayers = layers.Dense(64, activation="relu", name="hidden1")(inputs)
         hiddenLayers = layers.Dense(64, activation="relu", name="hidden2")(hiddenLayers)
-        #hiddenLayers = layers.Dense(4, activation="relu", name="hidden3")(hiddenLayers)
+        activationLayer = layers.Dense(action_size, activation="linear", name="activation")(hiddenLayers)
 
-        activationLayers = [layers.Dense(1, activation=keras.activations.linear, name="action_" + str(action))(hiddenLayers) for action in range(action_size)]
-
-        actionModels = [keras.models.Model(inputs=inputs, outputs=activationLayers[action], name="action_model_" + str(action)) for action in range(action_size)]
-
-        fullFinalLayer = keras.layers.Concatenate()(activationLayers)
-        fullModel = keras.models.Model(inputs=inputs, outputs=fullFinalLayer, name="full_model")
-
-        return actionModels, fullModel
+        return keras.models.Model(inputs=inputs, outputs=activationLayer, name="full_model")
 
     def step(self, state, action, reward, next_state, done):
         # Save experience in replay memory
@@ -79,7 +73,7 @@ class Agent():
             state (array_like): current state
             eps (float): epsilon, for epsilon-greedy action selection
         """
-        action_values = self.localFullModel(state.reshape([1,self.state_size]))[0].numpy()
+        action_values = self.localModel(state.reshape([1, self.state_size]))[0]
 
         # Epsilon-greedy action selection
         if random.random() > eps:
@@ -98,56 +92,30 @@ class Agent():
         #states, actions, rewards, next_states, dones = experiences
         #print("learning...")
 
-        '''
-        - wrap with gradient tape to keep track of operations on each tensor:
-
-run forward pass of each *individual* action model. The tensors used will be automatically recorded
-outputsForAction1 = actionModel1(action1Train, training=True)
-
-side by side, keep the expected value of each individual action model
-trueOutputsAction1
-
-all_train_y = concat(trueOutputsAction_i)
-all_outputs = concat(outputsForAction_i)
-
-full_loss = loss_fn(all_train_y, all_outputs)
-
--- come out of with gradient tape
-
-grads = tape.gradient(full_loss, fullModel.trainable_weights)'''
-
-        y_vals = [reward + gamma * self.targetFullModel(next_state.reshape([1,self.state_size]))[0].numpy().max() * done
+        y_vals = [reward + gamma * self.targetModel(next_state.reshape([1, self.state_size]))[0].numpy().max() * done
                   for state, action, reward, next_state, done in experiences]
-
-        experiencesPerAction = []
-        y_valsPerAction = []
-        for action in range(self.action_size):
-            experienceIndices = [i for i, experience in enumerate(experiences) if experience.action == action]
-            experiencesPerAction.append([experiences[i] for i in experienceIndices])
-            y_valsPerAction.append([y_vals[i] for i in experienceIndices])
-
+        '''
+                experiencesPerAction = []
+                y_valsPerAction = []
+                for action in range(self.action_size):
+                    experienceIndices = [i for i, experience in enumerate(experiences) if experience.action == action]
+                    experiencesPerAction.append([experiences[i] for i in experienceIndices])
+                    y_valsPerAction.append([y_vals[i] for i in experienceIndices])
+        '''
         with tf.GradientTape() as tape:
+            actionIndices = [[i, experience.action] for i, experience in enumerate(experiences)]
 
-            predictions = None
-            ordered_y_vals = []
+            fullPredictions = self.localModel(np.stack([state for state, _, _, _, _ in experiences]), training=True)
+            predictions = tf.gather_nd(fullPredictions,actionIndices)
 
-            for action in range(self.action_size):
-                startStates = [state for state, action, reward, next_state, done in experiencesPerAction[action]]
-                curPrediction = self.localActionModels[action](np.stack(startStates, axis=0), training=True)
-                if predictions is None:
-                    predictions = curPrediction
-                else:
-                    predictions = tf.concat([predictions, curPrediction], axis=0)
-                ordered_y_vals += y_valsPerAction[action]
+            loss_value = self.loss_fn(y_vals, predictions)
 
-            loss_value = self.loss_fn(ordered_y_vals, predictions)
-
-        grads = tape.gradient(loss_value, self.localFullModel.trainable_weights)
-        self.optimizer.apply_gradients(zip(grads, self.localFullModel.trainable_weights))
+        grads = tape.gradient(loss_value, self.localModel.trainable_weights)
+        self.optimizer.apply_gradients(zip(grads, self.localModel.trainable_weights))
 
         #print("done learning")
         # ------------------- update target network ------------------- #
-        self.update(self.localFullModel, self.targetFullModel)
+        self.update(self.localModel, self.targetModel)
 
 
     def update(self, local_model : keras.Sequential, target_model : keras.Sequential):
@@ -170,12 +138,12 @@ grads = tape.gradient(full_loss, fullModel.trainable_weights)'''
         #     target_param.data.copy_(tau*local_param.data + (1.0-TAU)*target_param.data)
 
     def save(self, pathPrefix : str):
-        self.localFullModel.save_weights(pathPrefix + ".ckpt")
+        self.localModel.save_weights(pathPrefix + ".ckpt")
         for i in range(self.action_size):
             self.localActionModels[i].save_weights(pathPrefix + "_" + str(i) + ".ckpt")
 
     def load(self, pathPrefix : str):
-        self.localFullModel.load_weights(pathPrefix + ".ckpt")
+        self.localModel.load_weights(pathPrefix + ".ckpt")
         for i in range(self.action_size):
             self.localActionModels[i].load_weights(pathPrefix + "_" + str(i) + ".ckpt")
 
